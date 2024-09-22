6ec25c4036cb7d09ed08005be337d0bd73c1b3ed
Udokikedaniel <danieludokike@gmail.com>
2024-09-22 18:42:56 +0100

    Using Postgresql

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLACHEMY_DATABASE_URL = "sqlite:///./todosapp.db"
+SQLACHEMY_DATABASE_URL = "postgresql://postgres:Command22__@localhost/Todoapplicationdatabase"
 
-engine = create_engine(SQLACHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(SQLACHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
